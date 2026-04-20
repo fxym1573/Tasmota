d69a9e8703d08dd2bdad510a024b7455a4194792
fxym1573 <113717364+fxym1573@users.noreply.github.com>
2026-04-20 20:38:20 +0800

    Update user_config_override.h for MQTT and rules

--- tasmota/user_config_override.h
+++ tasmota/user_config_override.h
@@ -5,7 +5,7 @@
 #define WIFI_SOFT_AP 1
 
 // --------------------------
-// 巴法云 MQTT（你提供的UID已填好）
+// 巴法云 MQTT
 // --------------------------
 #define MQTT_HOST       "bemfa.com"
 #define MQTT_PORT       9501
@@ -18,40 +18,39 @@
 #define MQTT_BUTTON_RETAIN 0
 
 // --------------------------
-// 你的开机卡硬件模板
+// 硬件模板
 // --------------------------
 #define MODULE 18
-#define USER_TEMPLATE "{\"NAME\":\"PC-CTRL\",\"GPIO\":[0,0,0,0,32,288,0,0,296,289,0,0,224,225,1,0],\"FLAG\":0,\"BASE\":18}"
+#define USER_TEMPLATE "{\"NAME\":\"PC-CTRL\",\"GPIO\":[0,0,0,0,32,289,0,0,290,291,0,0,225,226,1,0],\"FLAG\":0,\"BASE\":18}"
 
 // --------------------------
-// 功能开启
+// 功能
 // --------------------------
 #define USE_RULES 1
 #define USE_SWITCHES 1
 #define USE_OTA 1
+#define USE_OTA_FALLBACK 1
 
-// 关闭无用功能
 #undef USE_RF
 #undef USE_IR_REMOTE
 #undef USE_DOMOTICZ
 #undef USE_HOME_ASSISTANT
 
 // --------------------------
-// 【核心】巴法云指令规则
-// ON      = 开机  (IO12)
-// OFF     = 关机  (IO12)
-// restart = 重启  (IO13)
+// 【最终完整版规则】
+// ON = 开机
+// OFF = 关机
+// restart = 重启
+// upgrade xxx.bin = OTA远程升级
 // --------------------------
 #define USER_RULE1 \
 "ON MQTT#Received=ON DO Backlog Power1 1; Delay 5; Power1 0 ENDON " \
 "ON MQTT#Received=OFF DO Backlog Power1 1; Delay 5; Power1 0 ENDON " \
 "ON MQTT#Received=restart DO Backlog Power2 1; Delay 5; Power2 0 ENDON " \
 "ON System#Boot DO Backlog Delay 2; Power1 %Switch1% ENDON " \
-"ON Switch1#State DO Publish %topic% %value% ENDON "
+"ON Switch1#State DO Publish %topic% %value% ENDON " \
+"ON MQTT#Received~^upgrade DO OTA %value% ENDON "
 
 #define USER_RULE1_ON 1
 
-// --------------------------
-// 状态检测
-// --------------------------
 #define SWITCH_MODE1 1
