93e647e34e353f4dad6070a585f7888181891980
fxym1573 <113717364+fxym1573@users.noreply.github.com>
2026-04-20 20:50:22 +0800

    Update user_config_override.h for template and rules

--- tasmota/user_config_override.h
+++ tasmota/user_config_override.h
@@ -1,5 +1,5 @@
 // --------------------------
-// 双配网：SoftAP + SmartConfig
+// 双配网
 // --------------------------
 #define WIFI_SMARTCONFIG 1
 #define WIFI_SOFT_AP 1
@@ -20,37 +20,30 @@
 // 硬件模板
 // --------------------------
 #define MODULE 18
-#define USER_TEMPLATE "{\"NAME\":\"PC-CTRL\",\"GPIO\":[0,0,0,0,32,289,0,0,290,291,0,0,225,226,1,0],\"FLAG\":0,\"BASE\":18}"
+#define USER_TEMPLATE "{\"NAME\":\"PC-CTRL\",\"GPIO\":[0,0,0,0,32,288,0,0,296,289,0,0,224,225,1,0],\"FLAG\":0,\"BASE\":18}"
 
 // --------------------------
-// 功能开启
+// 功能
 // --------------------------
 #define USE_RULES 1
 #define USE_SWITCHES 1
 #define USE_OTA 1
-#define USE_OTA_FALLBACK 1
-#define USE_SERIAL_BRIDGE 1
 
-// 关闭无用功能
 #undef USE_RF
 #undef USE_IR_REMOTE
 #undef USE_DOMOTICZ
 #undef USE_HOME_ASSISTANT
 
 // --------------------------
-// 巴法云标准指令 + 升级日志打印
+// 巴法云标准指令集
 // --------------------------
 #define USER_RULE1 \
 "ON MQTT#Received=ON DO Backlog Power1 1; Delay 5; Power1 0 ENDON " \
 "ON MQTT#Received=OFF DO Backlog Power1 1; Delay 5; Power1 0 ENDON " \
 "ON MQTT#Received=restart DO Backlog Power2 1; Delay 5; Power2 0 ENDON " \
-"ON MQTT#Received=update DO Backlog SerialSend start update; OTA http://bin.bemfa.com/b/223505/1BcNjRmMjk2ZGIwOGQyNGQ0ZTkxYTE0ZmJmOTAwMWIzMjA=CZOKoZRmLb001.bin ENDON " \
+"ON MQTT#Received=ota DO OTA http://bin.bemfa.com/b/223505/1BcNjRmMjk2ZGIwOGQyNGQ0ZTkxYTE0ZmJmOTAwMWIzMjA=CZOKoZRmLb001.bin ENDON " \
 "ON System#Boot DO Backlog Delay 2; Power1 %Switch1% ENDON " \
-"ON Switch1#State DO Publish %topic% %value% ENDON " \
-"ON OTA#Started DO SerialSend CALLBACK: HTTP update process started ENDON " \
-"ON OTA#Progress DO SerialSend CALLBACK: HTTP update process at %value% bytes ENDON " \
-"ON OTA#Finished DO SerialSend CALLBACK: HTTP update process finished ENDON " \
-"ON OTA#Error DO SerialSend CALLBACK: HTTP update fatal error code %value% ENDON "
+"ON Switch1#State DO Publish %topic% %value% ENDON "
 
 #define USER_RULE1_ON 1
 #define SWITCH_MODE1 1
